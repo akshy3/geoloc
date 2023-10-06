4c2914544bb328ddd16bf9e5da93dbd55be8f777
akshay <akshayashok209@gmail.com>
2023-10-06 11:34:37 +0530

    Fixed issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import findLocation from "./assets/findLocation.svg";
 function ChangeView({ coords }: { coords: any }) {
   const map = useMap();
   map.setView(coords, 10);
@@ -32,44 +33,71 @@ function App() {
   return (
     <>
       {data && (
-        <div>
-          <input
-            type="text"
-            value={inputIp}
-            onChange={(e) => {
-              setInputIp(e.target.value);
-            }}
-          />
-          <button
-            type="submit"
-            onClick={() => {
-              fetchData(inputIp);
-            }}
-          >
-            locate
-          </button>
-          <hr />
-          <div>
-            <p>IP: {data.ip ?? 'Not available'}</p>
-            <p>Country: {data.country ?? 'Not available'}</p>
-            <p>Latitude: {data.latitude ?? 'Not available'}</p>
-            <p>Longitude: {data.longitude ?? 'Not available'}</p>
-            <p>City: {data.city ?? 'Not available'}</p>
-            <p>Region: {data.region ?? 'Not available'}</p>
-            <p>Organization: {data.organization ?? 'Not available'}</p>
+        <div className="container">
+          <div className="logo-container">
+            <span className="logo">GeoLoc</span>
           </div>
-          <MapContainer
-            className="map"
-            center={[data.latitude, data.longitude]}
-            zoom={10}
-            scrollWheelZoom={false}
-          >
-            <ChangeView coords={[data.latitude, data.longitude]} />
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+
+          <div className="input-container">
+            <input
+              type="text"
+              value={inputIp}
+              onChange={(e) => {
+                setInputIp(e.target.value);
+              }}
             />
-          </MapContainer>
+            <button
+              type="submit"
+              onClick={() => {
+                fetchData(inputIp);
+              }}
+            >
+              <img src={findLocation} />
+            </button>
+          </div>
+          <div className="ip-details">
+            <p>
+              <span className="label">IP:</span> {data.ip ?? "Not available"}
+            </p>
+            <p>
+              <span className="label">Country:</span>{" "}
+              {data.country ?? "Not available"}
+            </p>
+            <p>
+              <span className="label">Latitude:</span>{" "}
+              {data.latitude ?? "Not available"}
+            </p>
+            <p>
+              <span className="label">Longitude:</span>{" "}
+              {data.longitude ?? "Not available"}
+            </p>
+            <p>
+              <span className="label">City:</span>{" "}
+              {data.city ?? "Not available"}
+            </p>
+            <p>
+              <span className="label">Region:</span>{" "}
+              {data.region ?? "Not available"}
+            </p>
+            <p>
+              <span className="label">Organization:</span>{" "}
+              {data.organization ?? "Not available"}
+            </p>
+          </div>
+          <div className="map-container">
+            <MapContainer
+              className="map"
+              center={[data.latitude, data.longitude]}
+              zoom={10}
+              scrollWheelZoom={false}
+            >
+              <ChangeView coords={[data.latitude, data.longitude]} />
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+            </MapContainer>
+          </div>
         </div>
       )}
     </>
